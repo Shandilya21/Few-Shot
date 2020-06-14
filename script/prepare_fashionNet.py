85249e548cc4f5bddb271ea80979fbca40e54136
Arunav Shandilya <Arunav>
2020-06-15 03:10:40 +0530

    update: full and small version snippets added

--- script/prepare_fashionNet.py
+++ script/prepare_fashionNet.py
@@ -6,7 +6,6 @@ Run this script to prepare the Fashion dataset.
 2. Run the script
 
 """
-
 import sys
 sys.path.append('../')
 import os
@@ -32,21 +31,22 @@ print("Is the DATA_PATH is Correct?", os.path.exists(DATA_PATH + '/fashionNet/im
 Directory File Name Change
 1. styles.csv to map image_id and subCategory, class_laebls, meta_sets
 2. Rename the images using os.rename() for support and query split
-
 '''
+
 _classes = []
+split = []
 PATH = os.path.join(DATA_PATH, 'fashionNet/styles.csv')
 df = pd.read_csv(PATH, engine='python')
 df = df.drop(['Unnamed: 10', 'Unnamed: 11'], axis=1)
 df = df.sort_values(by=['id'], ascending=True, inplace=False)
-df['class_id'] = df.apply(lambda row: str(row['subCategory']) + '__' + str(row['articleType']) + '__' +  str(row['id']) + '.jpg', axis=1)
+df['class_id'] = df.apply(lambda row: str(row['articleType']) +  '__' +  str(row['id']) + '.jpg', axis=1)
 for name in df['class_id']:
 	_classes.append(name)
-
+ 
 '''
-_classes: ['Topwear__Tshirts__1163.jpg', 'Topwear__Tshirts__1164.jpg', 'Topwear__Tshirts__1165.jpg', 
-		'Bags__Backpacks__1525.jpg', 'Bags__Backpacks__1526.jpg']
-Format: [ subCategory__articleType__image_id]
+_classes: ['Tshirts__1163.jpg', 'Tshirts__1164.jpg', 'Bagpacks__1165.jpg', 
+		'Bagpacks__1525.jpg', 'Bagpacks__1526.jpg']
+Format: [ articleType__image_id]
 
 '''
 for root, _, files in os.walk(DATA_PATH + '/fashionNet/images/'):
@@ -62,35 +62,32 @@ for root, _, files in os.walk(DATA_PATH + '/fashionNet/refac_Images/'):
     for f in files:
         if f.endswith('.jpg'):
         	f = f.split('__')
-        	classes.append(f[0]+'__'+ f[1])
+        	classes.append(f[0])
 
 class_name = list(set(classes))
+# print(len(class_name))
 
 # Meta Training and Testing Split (Support and Query set)
 meta_train_PATH = DATA_PATH + '/fashionNet/Meta/meta_train.csv'
 meta_train_class = []
-df = pd.read_csv(meta_train_PATH, engine='python')
-for items in df['meta_train']:
+df_train = pd.read_csv(meta_train_PATH, engine='python')
+for items in df_train['meta_train']:
 	meta_train_class.append(items)
 
 meta_test_PATH = DATA_PATH + '/fashionNet/Meta/meta_test.csv'
 meta_test_class = []
-df = pd.read_csv(meta_test_PATH, engine='python')
-for items in df['meta_test']:
+df_test = pd.read_csv(meta_test_PATH, engine='python')
+for items in df_test['meta_test']:
 	meta_test_class.append(items)
 
-# Meta Train/Test Split (based on Meta Training and Testing set)
-background_classes = []
-for item in meta_train_class:
-	for substring in class_name:
-		if item in substring:
-			background_classes.append(substring)
+df_meta_training = df[df['articleType'].isin(meta_train_class)]
+meta_train_id = df_meta_training['articleType'].tolist()
+df_meta_testing = df[df['articleType'].isin(meta_test_class)]
+meta_test_id = df_meta_testing['articleType'].tolist()
 
-evaluation_classes = []
-for item in meta_test_class:
-	for substring in class_name:
-		if item in substring:
-			evaluation_classes.append(substring)
+
+background_classes = list(set(meta_train_id))
+evaluation_classes = list(set(meta_test_id))
 
 # Create class folders
 for c in background_classes:
@@ -104,8 +101,8 @@ for root, _, files in os.walk(DATA_PATH + '/fashionNet/refac_Images'):
     for f in tqdm(files, total=len(files)):
     	if f.endswith('.jpg'):
             name = f.split('__')
-            class_name = name[0] + '__' + name[1]
-            image_name = name[0] + '__' + name[1] + '__' + name[2]
+            class_name = name[0]
+            image_name = name[0] + '__' + name[1]
             # Send to correct folder
             if class_name not in (evaluation_classes + background_classes):
             	continue
